d3ceb8ad0f31f96f02b84df484947017beeaf349
Reem Ahmed <reem.mda0909@gmail.com>
2022-08-26 18:00:10 -0600

    create javascript file

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert!")
+// create prompt
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+// example consoles
+console.log("This logs a string, good for leaving yourself a message");
+//this will do the math
+console.log(30+10);
+//what is this?
+console.log("Our robot's name is " + playerName);
+
+// create fight function
+function fight() {
+    window.alert("The fight has begun!");
+}
+// fight();
