29a25758d6a6ddb3c3825393ae78048620765793
Reem Ahmed <reem.mda0909@gmail.com>
2022-08-29 12:46:13 -0600

    game round 1

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,50 @@
 // create prompt
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
-// example consoles
-console.log("This logs a string, good for leaving yourself a message");
-//this will do the math
-console.log(30+10);
-//what is this?
-console.log("Our robot's name is " + playerName);
+var playerHealth = 100;
+var playerAttack = 10;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
 // create fight function
-function fight() {
+var fight = function() {
+//Alert players that they're starting the round
     window.alert("The fight has begun!");
+};
+
+fight();
+
+// Subtract the value of playerAttack from the value of enemyHealth, and use that result to update the value in the enemyHealth variable.
+enemyHealth = enemyHealth-playerAttack;
+
+// Log a resulting message to the console to confirm that it worked.
+console.log(
+    playerName + " attacked " + enemyName + "." + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+//check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
 }
-// fight();
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
+// Subtract the value of enemyAttack from the value of playerHealth, and use that result to update the value in the playerHealth variable.
+playerHealth = playerHealth - enemyAttack;
+// Log a resulting message to the console to confirm that it worked.
+
+console.log(
+    enemyName + " attacked " + playerName + "." +  playerName + " now has " + playerHealth + " health remaining."
+);
+
+// check players health
+if(playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+
+}
+// log a resulting message to the console to confirm that it worked.
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+}
